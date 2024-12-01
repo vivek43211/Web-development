bb5c312b2f2c8077a9aed2f6084590c0be3f0463
vivekvyas <vyasvivek670@gmail.com>
2024-12-01 21:54:16 +0530

    react

---
+++ ViteReact/02vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+   return(
+<h1>
+    Hello World  || Vivek vyas
+    vite is framework have same work as utlies . npm create Vite@latest use to sytnax to start the vite-react . 
+    its have 2 rules to define the function that function have jsx at the end of file and function have uppercase 
+    
+ </h1>
+   )
+ 
+  
+}
+
+export default App
