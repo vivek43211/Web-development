724d488f261a2ff95ec9df7ff29cb13d8ea1b7ef
vivekvyas <vyasvivek670@gmail.com>
2024-12-02 17:53:51 +0530

    chai.js

--- ViteReact/02vitereact/src/App.jsx
+++ ViteReact/02vitereact/src/App.jsx
@@ -1,15 +1,6 @@
-
+import Chai from './Chai.jsx'
 function App() {
-   return(
-<h1>
-    Hello World  || Vivek vyas
-    vite is framework have same work as utlies . npm create Vite@latest use to sytnax to start the vite-react . 
-    its have 2 rules to define the function that function have jsx at the end of file and function have uppercase 
-    
- </h1>
-   )
- 
-  
+   <Chai/>
 }
 
 export default App
